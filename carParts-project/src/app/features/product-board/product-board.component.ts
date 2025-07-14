99d806b48241922e963bff5efaa7cab276c2e51a
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-07-14 22:29:07 +0300

    Create product-board

---
+++ carParts-project/src/app/features/product-board/product-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-board',
+  imports: [],
+  templateUrl: './product-board.component.html',
+  styleUrl: './product-board.component.css'
+})
+export class ProductBoardComponent {
+
+}
