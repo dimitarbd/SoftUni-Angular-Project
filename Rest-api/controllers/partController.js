bffeb321ad26408f277a90fe1593a45ccd92c1ed
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-07-28 23:37:59 +0300

    Create partController

---
+++ Rest-api/controllers/partController.js
@@ -0,0 +1,52 @@
+const { themeModel } = require('../models');
+const { newPost } = require('./postController')
+
+function getParts(req, res, next) {
+    partModel.find()
+        .populate('userId')
+        .then(parts => res.json(parts))
+        .catch(next);
+}
+
+function getPart(req, res, next) {
+    const { partId } = req.params;
+
+    partModel.findById(partId)
+        .populate({
+            path : 'parts',
+            populate : {
+              path : 'userId'
+            }
+          })
+        .then(part => res.json(part))
+        .catch(next);
+}
+
+function createPart(req, res, next) {
+    const { partName, partText } = req.body;
+    const { _id: userId } = req.user;
+
+    partModel.create({ partName, userId, subscribers: [userId] })
+        .then(part => {
+            newPost(partText, userId, part._id)
+                .then(([_, updatedPart]) => res.status(200).json(updatedPart))
+        })
+        .catch(next);
+}
+
+function updatePart(req, res, next) {
+    const partId = req.params.partId;
+    const { _id: userId } = req.user;
+    partModel.findByIdAndUpdate({ _id: partId }, { $addToSet: { subscribers: userId } }, { new: true })
+        .then(updatedPart => {
+            res.status(200).json(updatedPart)
+        })
+        .catch(next);
+}
+
+module.exports = {
+    getParts,
+    createPart,
+    getPart,
+    updatePart,
+}
