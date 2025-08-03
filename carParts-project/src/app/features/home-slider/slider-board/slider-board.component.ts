66eb7465b6cf36ff975f37024b75a88c1b0ffe09
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-08-03 18:26:41 +0300

    Create home page slider

---
+++ carParts-project/src/app/features/home-slider/slider-board/slider-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-board',
+  imports: [],
+  templateUrl: './slider-board.component.html',
+  styleUrl: './slider-board.component.css'
+})
+export class SliderBoardComponent {
+
+}
