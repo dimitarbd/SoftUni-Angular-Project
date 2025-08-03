ccf932fef6c4ef4e854f114aa46a0da3c6ebb4c6
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-08-03 11:30:09 +0300

    Update part.service

--- carParts-project/src/app/core/services/part.service.ts
+++ carParts-project/src/app/core/services/part.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Part} from "../../models";
+import { Part } from "../../models";
 import { Observable } from "rxjs";
 
 @Injectable({
@@ -9,9 +9,13 @@ import { Observable } from "rxjs";
 export class PartService {
     private apiUrl = 'http://localhost:3000/api/parts?limit={0}';
 
-    constructor(private httpClient: HttpClient) {}
+    constructor(private httpClient: HttpClient) { }
 
-        getRecentParts(limit: number = 10): Observable<Part[]> {
+    getParts(): Observable<Part[]> {
+        return this.httpClient.get<Part[]>(this.apiUrl);
+    }
+
+    getRecentParts(limit: number = 10): Observable<Part[]> {
         return this.httpClient.get<Part[]>(this.apiUrl.replace('{0}', limit.toString()));
     }
 
