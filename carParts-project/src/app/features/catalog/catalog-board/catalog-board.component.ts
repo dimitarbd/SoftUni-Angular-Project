29169c310e3b28e23bec954dfba811b8f3ec68a3
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-08-04 21:18:52 +0300

    Create catalog-board component

---
+++ carParts-project/src/app/features/catalog/catalog-board/catalog-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-board',
+  imports: [],
+  templateUrl: './catalog-board.component.html',
+  styleUrl: './catalog-board.component.css'
+})
+export class CatalogBoardComponent {
+
+}
