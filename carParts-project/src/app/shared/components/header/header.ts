59bb05c31fb9395835d34786db570c12a6c623ba
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-08-09 23:05:11 +0300

    Update header component

--- carParts-project/src/app/shared/components/header/header.ts
+++ carParts-project/src/app/shared/components/header/header.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class HeaderComponent {
+  isCategoriesOpen = false;
 
+  toggleCategories(): void {
+    this.isCategoriesOpen = !this.isCategoriesOpen;
+  }
 }
