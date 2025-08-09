36861c2b222b293cce548c8c272f382ea9027fda
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-08-09 17:37:44 +0300

    Create shippin component

--- carParts-project/src/app/features/home/home.ts
+++ carParts-project/src/app/features/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NewProductBoard } from '../new-products/new-product-board/new-product-board';
 import { SliderBoardComponent } from '../home-slider/slider-board/slider-board';
+import { ShippingComponent } from '../../shared/components/shipping/shipping';
 
 @Component({
   selector: 'app-home',
-  imports: [SliderBoardComponent, NewProductBoard],
+  imports: [SliderBoardComponent, NewProductBoard, ShippingComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
