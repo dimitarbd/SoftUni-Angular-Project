9006d1b127344e2a062d0e0ec30d5a7475014882
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-08-11 20:49:37 +0300

    Remove Rest-api

--- Rest-api/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const authController = require('./auth');
-const commentController = require('./commentController');
-const partController = require('./partController');
-
-module.exports = {
-    authController,
-    commentController,
-    partController,
-}
