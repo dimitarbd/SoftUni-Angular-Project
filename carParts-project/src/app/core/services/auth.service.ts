929ccc16875f283d88d535cbbaf6a606577ef352
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-08-11 23:49:30 +0300

    Update auth.service.ts

--- carParts-project/src/app/core/services/auth.service.ts
+++ carParts-project/src/app/core/services/auth.service.ts
@@ -12,29 +12,61 @@ export class AuthService {
             _id: '1',
             email: 'test@test.com',
             password: '123456',
-            name: 'Test',
-            role: 'user'
+            username: 'Test',
         },
         {
             _id: '2',
             email: 'admin@admin.com',
             password: '123456',
-            name: 'Admin',
-            role: 'admin'
+            username: 'Admin',
         }
     ];
 
-
-    login(email: string, password: string): void {
-        this._isLoggedIn.set(true);
+    constructor() {
+        const savedUser = localStorage.getItem('currentUser');
+        if (savedUser) {
+            const user = JSON.parse(savedUser);
+            this._currentUser.set(user);
+            this._isLoggedIn.set(true);
+        } 
     }
 
-    register(email: string, password: string): void {
-        this._isLoggedIn.set(true);
+
+    login(email: string, password: string): boolean {
+        const user = this._users.find(user => user.email === email && user.password === password);
+        if (user) {
+            this._currentUser.set(user);
+            this._isLoggedIn.set(true);
+
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            return true;
+        }
+        return false;
+    }
+
+    register(email: string, password: string, rePassword: string): boolean {
+        if (email && password && rePassword && password === rePassword) {
+            const newUser: User = {
+                _id: 'user_${Date.now()}',
+                email: email,
+                password: password,
+            };
+
+            this._users.push(newUser);
+            this._currentUser.set(newUser);
+            this._isLoggedIn.set(true);
+
+            localStorage.setItem('currentUser', JSON.stringify(newUser));
+
+            return true;
+        }
+        return false;
     }
 
     logout(): void {
+        this._currentUser.set(null);
         this._isLoggedIn.set(false);
+        localStorage.removeItem('currentUser');
     }
 
 }
