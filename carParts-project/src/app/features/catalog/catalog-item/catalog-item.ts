4ef33ed2dffd3219ad2e0d8cf2a7d330875e4a5b
Dimitar Dimitrov <146258599+dimitarbd@users.noreply.github.com>
2025-08-14 21:40:48 +0300

    Update catalog component

--- carParts-project/src/app/features/catalog/catalog-item/catalog-item.ts
+++ carParts-project/src/app/features/catalog/catalog-item/catalog-item.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Part } from '../../../models/part.model';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-catalog-item',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, RouterLink ],
   templateUrl: './catalog-item.html',
-  styleUrl: './catalog-item.css'
+  styleUrl: './catalog-item.css',
+  host: { class: 'col-lg-4 col-md-6 col-sm-6' }
 })
 
 export class CatalogItem {
